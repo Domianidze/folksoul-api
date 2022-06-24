d42aecfbd282483025ecb38059a58777c693663b
Domianidze <sdomianidze123@gmail.com>
2022-06-24 16:00:14 +0400

    feat: add change icon endpoint

--- src/controllers/social-media-controller.ts
+++ src/controllers/social-media-controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction } from 'express'
+import fs from 'fs'
 
 import { SocialMedia } from '../models'
 import { socialMediaSchema } from '../schemas'
-import { getDefaultImagePath } from '../util'
+import { ErrorType } from '../types'
+import { getDefaultImagePath, getImagePath } from '../util'
 
 export const addSocialMedia = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -17,6 +19,44 @@ export const addSocialMedia = async (req: Request, res: Response, next: NextFunc
     res.status(201).json({
       message: 'Social Media added successfully!',
       memberId: response._id,
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const changeIcon = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const socialMedia = await SocialMedia.findById(req.body.socialMediaId)
+
+    if(!req.file) {
+      const error: ErrorType = new Error("Proper image is required.")
+      error.statusCode = 422
+      throw error
+    }
+
+    if (!socialMedia) {
+      fs.unlinkSync(req.file.path)
+      
+      const error: ErrorType = new Error("Social media with this id not found.")
+      error.statusCode = 404
+      throw error
+    }
+
+    if(socialMedia.iconUrl && socialMedia.iconUrl !== getDefaultImagePath('social-media')) {
+      const path = getImagePath(socialMedia.iconUrl)
+
+      if(fs.existsSync(path)) {
+        fs.unlinkSync(path)
+      } 
+    }
+
+    socialMedia.iconUrl = `http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/${req.file.path}`
+
+    await socialMedia.save()
+
+    res.status(200).json({
+      message: 'Icon changed successfully!',
     })
   } catch (err) {
     next(err)
--- src/routes/social-media-routes.ts
+++ src/routes/social-media-routes.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 
-import { addSocialMedia } from '../controllers'
+import { addSocialMedia, changeIcon } from '../controllers'
 
 const Router = express.Router()
 
 Router.post('/add-social-media', addSocialMedia)
+Router.put('/change-icon', changeIcon)
 
 export default Router
