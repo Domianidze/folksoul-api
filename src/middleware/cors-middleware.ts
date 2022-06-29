ecf681c201bad5f1b129e5236f5f142da708b912
Domianidze <sdomianidze123@gmail.com>
2022-06-29 15:10:42 +0400

    fix: cors methods

--- src/middleware/cors-middleware.ts
+++ src/middleware/cors-middleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express'
 
 export default (_: Request, res: Response, next: NextFunction) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     next()
 }
